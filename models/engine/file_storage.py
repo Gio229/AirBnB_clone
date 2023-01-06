9cade86075bc5b456e7524194df65ddee5644ef3
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-01-06 20:16:29 +0100

    file satorage setting up

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,17 +10,47 @@ class FileStorage:
     Serializes instances to a JSON file and
     deserializes JSON file to instances
     """
+
+    # path to the JSON file storage
     __file_path = "file.json"
+    # dictionary which will contains all created objects
     __objects = {}
 
     def all(self):
-        pass
+        """Returns the dictionary __objects
+        """
+        return self.__objects
 
     def new(self, obj):
-        pass
+        """Add to __objects a new object
+        """
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
+            self.__objects[key] = obj
 
     def save(self):
-        pass
+        """Save objects to json file storage
+        """
+        print("enter in save")
+        try:
+            objects_to_store = {}
+            for key, value in self.__objects.items():
+                objects_to_store[key] = value.to_dict()
+            with open(self.__file_path, 'w') as f:
+                json.dump(objects_to_store, f)
+                print(" tout bien {}".format(objects_to_store))
+        except:
+            return
+        print("out of save")
 
     def reload(self):
-        pass
+        """Recovered all objects from json file storage
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                recovered_objects = json.load(f)
+                for object in recovered_objects.values():
+                    del object["__class__"]
+                self.__objects = recovered_objects
+        except:
+            return
