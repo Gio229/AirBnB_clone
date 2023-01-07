dec12d9a000d6bf934b6b65cb01a1982a4c89ef0
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-01-07 13:26:00 +0100

    working on test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,7 +13,29 @@ class TestBaseModel(unittest.TestCase):
     """
 
     def test_instantiation(self):
+        """Test if instance have the right type
+        """
         self.assertEqual(BaseModel, type(BaseModel()))
+
+    def test_constructor(self):
+        """Test if constructor work well
+        """
+        obj1 = BaseModel()
+        dict_obj1 = obj1.to_dict()
+        obj2 = BaseModel(**dict_obj1)
+        self.assertEqual(obj1.id, obj2.id)
+        self.assertEqual(obj1.created_at, obj2.created_at)
+        self.assertEqual(obj1.updated_at, obj2.updated_at)
+    
+    def test_save(self):
+        """Test if instance is correctly saved
+        """
+        baseModel = BaseModel()
+        old_updated_at = baseModel.updated_at
+        baseModel.save()
+        new_updated_at = baseModel.updated_at
+        self.assertNotEqual(old_updated_at, new_updated_at)
+
 
 
 if __name__ == "__main__":
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,7 +5,15 @@
 Contains the TestFileStorageDocs classes
 """
 import unittest
+import json
 
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from models.engine.file_storage import FileStorage
 
 
@@ -22,8 +30,34 @@ class TestFileStorageDocs(unittest.TestCase):
     def test_all(self):
         """Test if all method work well"""
         storage = FileStorage()
-        objects_from_storage = storage.all()
-        self.assertEqual(type(objects_from_storage), dict)
+        objects = storage.all()
+        self.assertEqual(type(objects), dict)
+
+    def test_reload(self):
+        storage = FileStorage()
+        baseModel = BaseModel()
+        storage.new(baseModel)
+        storage.save()
+        storage.reload()
+        self.assertIn("BaseModel." + baseModel.id,
+                      storage.all())
+    
+    def test_new(self):
+        storage = FileStorage()
+        baseModel = BaseModel()
+        storage.new(baseModel)
+        self.assertIn("BaseModel." + baseModel.id,
+                      storage.all())
+
+    def test_save(self):
+        storage = FileStorage()
+        baseModel = BaseModel()
+        storage.new(baseModel)
+        storage.save()
+        with open("file.json", "r") as f:
+            recovered_objects = json.load(f)
+            self.assertIn("BaseModel." + baseModel.id, recovered_objects)
+
 
 if __name__ == "__main__":
     unittest.main()
