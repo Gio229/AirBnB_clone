e7c98205202a65afb4ecce71e8358317e47b221b
ATCHAOUE Giovanni <aagmaougnon@gmail.com>
2023-01-08 09:06:49 +0100

    testing user attributes

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -3,6 +3,7 @@
 This module define the Place
 """
 import unittest
+from datetime import datetime
 
 from models.user import User
 
@@ -13,7 +14,33 @@ class TestUser(unittest.TestCase):
     """
 
     def test_instantiation(self):
-        self.assertEqual(User, type(User()))
+        """Test if new instance have the right type
+        """
+        user = User()
+        self.assertEqual(User, type(user))
+        self.assertTrue(type(user.id) is str)
+        self.assertTrue(type(user.created_at) is datetime)
+        self.assertTrue(type(user.updated_at) is datetime)
+
+    def test_email(self):
+        """Test if email attribute is string
+        """
+        self.assertEqual(str, type(User.email))
+
+    def test_password(self):
+        """Test if password attribute is string
+        """
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name(self):
+        """Test if first_name attribute is string
+        """
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name(self):
+        """Test if last_name attribute is string
+        """
+        self.assertEqual(str, type(User.last_name))
 
 
 if __name__ == "__main__":
